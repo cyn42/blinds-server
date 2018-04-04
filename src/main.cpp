5bf3b74d085367bb7cae45be048939b1a52964d4
Cyn42 <cyntaylor42@gmail.com>
2018-04-04 19:32:31 -0400

    Renamed config to cfg, moved from NodeMCU to Feather Huzzah ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,17 @@
  * First Drop: Get it working with all code in one file. Then make it pretty
  */
 #include "Arduino.h"
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <config.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <cfg.h>
 
 const char* ssid = WIFISSID;
 const char* password = WIFIPASSWORD;
 
 String webString="";     // String to display
 
-ESP8266WebServer server(80);
-
-void handle_root() {
-  server.send(200, "text/plain", "Hello from the blinds server, control from /blinds/control");
-  delay(100);
-}
+AsyncWebServer server(80);
 
 void setup(void)
 {
@@ -46,11 +40,13 @@ void setup(void)
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handle_root);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Hello from the blinds server, control from /blinds/control");
+  });
 
-  server.on("/blinds/control", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+  server.on("/blinds/control",  HTTP_GET, [](AsyncWebServerRequest *request){
     webString="Control the Blinds";
-    server.send(200, "text/plain", webString);               // send to someones browser when asked
+    request->send(200, "text/plain", webString);               // send to someones browser when asked
   });
 
   server.begin();
@@ -59,5 +55,5 @@ void setup(void)
 
 void loop(void)
 {
-  server.handleClient();
+  //server.handleClient();
 }
