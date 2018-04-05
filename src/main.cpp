d80c0b3f8ae3c15a8f50129d66a9d836a4720178
Cyn42 <cyntaylor42@gmail.com>
2018-04-04 20:15:00 -0400

    Added Connecting / Connected LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,33 @@
 const char* ssid = WIFISSID;
 const char* password = WIFIPASSWORD;
 
+int connectingLED = 15;
+int connectedLED = 14;
+
+
 String webString="";     // String to display
 
 AsyncWebServer server(80);
 
 void setup(void)
 {
-  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
-  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+  Serial.begin(115200);
 
-  // Connect to WiFi network
+  pinMode(connectingLED, OUTPUT);
+  digitalWrite(connectingLED, HIGH);
+
+  pinMode(connectedLED, OUTPUT);
+  digitalWrite(connectedLED, LOW);
+
   WiFi.begin(ssid, password);
   Serial.print("\n\r \n\rWorking to connect");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    digitalWrite(connectingLED, LOW);
+    delay(250);
+    digitalWrite(connectingLED, HIGH);
+    delay(250);
     Serial.print(".");
   }
   Serial.println("");
@@ -39,6 +50,8 @@ void setup(void)
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  digitalWrite(connectedLED, HIGH);
+  digitalWrite(connectingLED, LOW);
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/plain", "Hello from the blinds server, control from /blinds/control");
